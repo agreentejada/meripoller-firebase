c7d602af5efcba161452de058d656457b4fc3cef
unknown <ramsin97@outlook.comm>
2020-11-08 13:05:39 -0800

    Initial commit

---
+++ functions/src/index.ts
@@ -0,0 +1,9 @@
+import * as functions from 'firebase-functions';
+
+export const myBot = functions.https.onRequest((req, res) => {
+    //Request from Slack
+    const { challenge } = req.body;
+
+  //Response from You.
+  res.send({challenge});
+});
